990e0144820b527c9a7f2c45a6b0314801ce3fc0
Dionisius Rinaldi Sallu <105428579+Hienz0@users.noreply.github.com>
2024-11-07 22:48:49 +0800

    Separate search into new component
    - Just need to call the component
    - Need category adjustment

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent {
+  searchTerm: string = '';
+  selectedCategory: string = 'Accommodation';
 
+  constructor(
+    private router: Router,
+  ) {}
+  performSearch(): void {
+    console.log('category: ', this.selectedCategory);
+    console.log('search term: ', this.searchTerm);
+    if (this.searchTerm.trim().length > 2) {
+      let queryParams: any = { searchTerm: this.searchTerm };
+      if (this.selectedCategory) {
+        queryParams.category = this.selectedCategory;
+      }
+      this.router.navigate(['/search-results'], { queryParams: queryParams });
+    }
+  }
+
+  searchInCategory(category: string): void {
+    this.selectedCategory = category;
+    this.performSearch(); // Perform search when a category is selected
+  }
 }
